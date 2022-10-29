fbceaa267aa63b14734914d233efb66c17ac3525
Anton Plesca <anton.plesca@yahoo.com>
2022-10-29 10:59:32 -0600

    replaced the campsideCard with a CampsitesList to have 4 images displayed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
+import CampsitesList from './features/campsites/CampsitesList';
 
 function App() {
     return (
@@ -13,7 +14,7 @@ function App() {
                     </NavbarBrand>
                 </Container>
             </Navbar>
-            I'm ready for workshop!
+            <CampsitesList />
         </div>
     );
 }
