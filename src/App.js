a90f61bd42d91eacfe7ebb43a01835fcc9b16210
Anton Plesca <anton.plesca@yahoo.com>
2022-11-06 21:03:58 -0700

    added community partners componenet to about us page and featured partner component on the main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
         <Header />
-        <CampsitesDirectoryPage />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='about' element={<AboutPage />} />
+          <Route path='contact' element={<ContactPage />} />
+          <Route path='directory' element={<CampsitesDirectoryPage />} />
+          <Route 
+              path='directory/:campsiteId'
+              element={<CampsiteDetailPage />}
+          />
+        </Routes>
         <Footer />
     </div>
   );
