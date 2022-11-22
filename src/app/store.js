032377f6f9e5a4c9703f927cb164d03bee6c1dc5
Anton Plesca <anton.plesca@yahoo.com>
2022-11-21 19:06:55 -0700

    created a Login form which validates user input

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import { userReducer } from '../features/user/userSlice';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
@@ -10,7 +11,8 @@ export const store = configureStore({
         campsites: campsitesReducer,
         comments: commentsReducer,
         partners: partnersReducer,
-        promotions: promotionsReducer
+        promotions: promotionsReducer,
+        user: userReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
